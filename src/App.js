00442966f04af0eacea7b1a7dba13637aa484d19
6210110104 <6210110104@psu.ac.th>
2022-08-17 20:45:11 +0700

    HW-Commit1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import CharacterCard from './CharacterClass';
 import WordCard from './WordCard';
 
+
 const word = "Hello";
 function App() {
+  const textInput = <input type="text"/>;
   return (
     <div>
       {
         <WordCard value = {word}/>
       }
+      <p>
+        {textInput}
+      </p>
     </div>
   );
 }
