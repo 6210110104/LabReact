07b3ab3b81e90c09baa08113565a3db56cddd78a
6210110104 <6210110104@psu.ac.th>
2022-08-20 22:15:08 +0700

    HW-Commit4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import { random } from 'lodash';
 import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterClass';
 import WordCard from './WordCard';
 
-
-const word = "Hello";
+var word = "Hello"
 function App() {
   const textInput = <input type="text"/>;
   return (
     <div>
       {
-        <WordCard value = {word}/>
+      <WordCard value = {word}/>
       }
       <p>
         Guess the word:
